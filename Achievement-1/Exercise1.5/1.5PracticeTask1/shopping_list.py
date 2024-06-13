e6788b35bf81344a19ce314ecbc2af4b5acb78f7
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-06-13 15:09:28 -0600

    Practice Task 1

---
+++ Achievement-1/Exercise1.5/1.5PracticeTask1/shopping_list.py
@@ -0,0 +1,42 @@
+# Creating a class
+class ShoppingList(object):
+    def __init__(self, list_name):
+        self.list_name = list_name
+        self.shopping_list = []
+       
+# Methods to add and remove items 
+    def add_item(self, item):
+        if item not in self.shopping_list:
+            self.shopping_list.append(item)
+            print(f'{item} has been added to the shopping list!')
+        else:
+            print(f'{item} already exists!')
+            
+    def remove_item(self, item):
+        if item in self.shopping_list:
+            self.shopping_list.remove(item)
+            print(f'{item} removed from the shopping list!')
+        else:
+            print(f'{item} does not exist in the shopping list!')
+            
+    def view_list(self):
+        if not self.shopping_list:
+            print('The shopping list is empty.')
+        else:
+            print(f'{self.list_name}:')
+            for item in self.shopping_list:
+                print(f'{item}')
+
+pet_store_list = ShoppingList('Pet Store Shopping List')
+
+pet_store_list.add_item('dog food')
+pet_store_list.add_item('frisbee')
+pet_store_list.add_item('bowl')
+pet_store_list.add_item('collars')
+pet_store_list.add_item('flea collars')
+
+pet_store_list.remove_item('flea collars')
+
+pet_store_list.add_item('frisbee')
+
+pet_store_list.view_list()
